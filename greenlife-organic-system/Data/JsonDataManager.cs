765f1812cf3c5873bb7da20db9537028708fdc60
Salman-Faizal <salmanfaizal1000@gmail.com>
2026-02-08 23:04:22 +0530

    Added json data manager class, and a folder to store json files.

---
+++ greenlife-organic-system/Data/JsonDataManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace greenlife_organic_system.Data
+{
+    public static class JsonDataManager
+    {
+        public static void SaveToFile<T>(string filePath, List<T> data)
+        {
+            string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+            File.WriteAllText(filePath, json);
+        }
+
+        public static List<T> LoadFromFile<T>(string filePath)
+        {
+            if (!File.Exists(filePath))
+                return new List<T>();
+
+            string json = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<List<T>>(json);
+        }
+    }
+}
